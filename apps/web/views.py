baf5871bc16e8da88b763a8ba88629e8733dbd34
Ruben <rubengza.98@gmail.com>
2021-09-17 01:29:08 -0600

    Modified index again, change database, change views

--- apps/web/views.py
+++ apps/web/views.py
@@ -4,12 +4,3 @@ from .models import *
 def index(request):
     allpet = pet.objects.all()
     return render(request, 'index.html', {'allpet':allpet})
-
-def about(request):
-    return render(request, 'about .html')
-
-def blog(request):
-    return render(request, 'blog.html')
-
-def contact(request):
-    return render(request, 'contact.html')
